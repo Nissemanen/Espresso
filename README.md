0e0b32e6ccd60a28aa9fad56fa45249898551f8d
Nissemanen <nisse.malmstrom@icloud.com>
2026-04-15 16:20:21 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Espresso
+
+A small and open source java game engine, focused on making games in java.
---
+++ src/main/java/io/github/nissemanen/Main.java
@@ -0,0 +1,8 @@
+package io.github.nissemanen;
+
+
+public class Main {
+    static void main() {
+        // Nothing here yet
+    }
+}
