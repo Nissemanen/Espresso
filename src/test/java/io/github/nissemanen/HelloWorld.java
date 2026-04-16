4dd2bbd42a9ce3696e37bd1c4473fde8c1e2ce4f
nisse <nisse.malmstrom@icloud.com>
2026-04-16 14:52:49 +0200

    why doesnt maven just install the god damn dependencies???? like 'oh i cant build without these, wah wah wah' JUST INSTALL THEM FOR GOD SAKE. im switching to my pc now, hopefully its more compitent than my mac

--- src/test/java/io/github/nissemanen/HelloWorld.java
+++ src/test/java/io/github/nissemanen/HelloWorld.java
@@ -1,4 +1,11 @@
 package io.github.nissemanen;
 
+import java.nio.*;
+
 public class HelloWorld {
+    public long window;
+
+    public void run() {
+        System.out.println("Hello ");
+    }
 }
